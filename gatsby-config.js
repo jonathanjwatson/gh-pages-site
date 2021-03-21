791675f4e4a96d0177698745a0f1bee8de86d294
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-03-20 21:26:31 -0400

    trying to get module exports to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/gatsby-pages-site`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
